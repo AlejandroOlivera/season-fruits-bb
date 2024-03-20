e51ba72a4787a7f2cab9a90d17165f4bca19e77d
Alejandro Olivera <aienma325@gmail.com>
2024-03-19 20:15:59 -0500

    test coors

--- src/api.ts
+++ src/api.ts
@@ -1,25 +1,16 @@
 import axios from 'axios';
 
 const baseUrl = import.meta.env.VITE_APP_BASE_URL;
-const instance = axios.create({
-  baseURL: baseUrl,
-  headers: {
-    Accept:
-      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
-    Connection: 'keep-alive',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'Upgrade-Insecure-Requests': '1',
-  },
-});
+
 export const fetchAllFruits = async () => {
   try {
-    const response = await instance.get(`${baseUrl}/fruit/all`);
-    return response.data;
+    const response = await axios.get(`${baseUrl}/fruit/all`, {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+      },
+    });
+    return response;
   } catch (error) {
     console.error('Error fetching fruits:', error);
     throw error;
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://fruityvice.com/',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     react(),
     ViteImageOptimizer({
